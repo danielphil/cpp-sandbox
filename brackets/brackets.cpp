b0f26b29a9a2c43a5b1e259c99b1cb48d8a559e8
Daniel Phillips <dphildev@gmail.com>
2016-12-29 19:25:31 +0000

    Some bracket expression parsing code.

--- brackets/brackets.cpp
+++ brackets/brackets.cpp
@@ -1,2 +1,67 @@
 #include <iostream>
+#include <stack>
 #include "gtest/gtest.h"
+
+bool IsBalanced(const std::string& expression, int& failure_index);
+
+bool IsBalanced(const std::string& expression, int& failure_index) {
+    std::stack<int> stack;
+
+    for (int i = 0; i < expression.size(); i++) {
+        char c = expression[i];
+
+        if (c == '(') {
+            stack.push(i);
+        } else if (c == ')') {
+            if (stack.empty()) {
+                failure_index = i;
+                return false;
+            }
+
+            stack.pop();
+        }
+    }
+
+    if (stack.empty()) {
+        return true;
+    } else {
+        while (!stack.empty()) {
+            failure_index = stack.top();
+            stack.pop();
+        }
+        return false;
+    }
+}
+
+TEST(Brackets, BalancedExpressions) {
+    int failure_index;
+    EXPECT_TRUE(IsBalanced("(()())()", failure_index));
+    EXPECT_TRUE(IsBalanced("", failure_index));
+}
+
+TEST(Brackets, TooManyClosing) {
+    int failure_index;
+    EXPECT_FALSE(IsBalanced("(())())()", failure_index));
+    EXPECT_EQ(6, failure_index);
+
+    EXPECT_FALSE(IsBalanced(")", failure_index));
+    EXPECT_EQ(0, failure_index);
+
+    EXPECT_FALSE(IsBalanced("())", failure_index));
+    EXPECT_EQ(2, failure_index);
+}
+
+TEST(Brackets, TooManyOpening) {
+    int failure_index;
+    EXPECT_FALSE(IsBalanced("(", failure_index));
+    EXPECT_EQ(0, failure_index);
+
+    EXPECT_FALSE(IsBalanced("()(()(())", failure_index));
+    EXPECT_EQ(2, failure_index);
+
+    EXPECT_FALSE(IsBalanced("(()", failure_index));
+    EXPECT_EQ(0, failure_index);
+
+    EXPECT_FALSE(IsBalanced("(()())(((((((((", failure_index));
+    EXPECT_EQ(6, failure_index);
+}
