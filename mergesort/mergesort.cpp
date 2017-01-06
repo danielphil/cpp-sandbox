949f3bb2fcdede182170dd8a8264c35b46a4f5ad
Daniel Phillips <dphildev@gmail.com>
2017-01-06 10:38:32 +0000

    Simple mergesort

---
+++ mergesort/mergesort.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+#include "gtest/gtest.h"
+
+template <typename T>
+void merge(
+    std::vector<T>& items,
+    int a_index,
+    int a_size,
+    int b_index,
+    int b_size
+) {
+    int dest_index = a_index;
+
+    std::deque<T> a(items.begin() + a_index, items.begin() + a_index + a_size);
+    std::deque<T> b(items.begin() + b_index, items.begin() + b_index + b_size);
+
+    while (!a.empty() && !b.empty()) {
+        if (a.front() <= b.front()) {
+            items[dest_index] = a.front();
+            a.pop_front();
+        } else {
+            items[dest_index] = b.front();
+            b.pop_front();
+        }
+
+        dest_index++;
+    }
+
+    while (!a.empty()) {
+        items[dest_index] = a.front();
+        a.pop_front();
+        dest_index++;
+    }
+
+    while (!b.empty()) {
+        items[dest_index] = b.front();
+        b.pop_front();
+        dest_index++;
+    }
+}
+
+template <typename T>
+void mergesort(std::vector<T>& items, int start_index, int items_to_sort) {
+    if (items_to_sort <= 1) {
+        return;
+    }
+
+    const int first_chunk_size = items_to_sort / 2;
+    const int second_chunk_size = items_to_sort - first_chunk_size;
+    const int b_index = start_index + first_chunk_size;
+    mergesort(items, start_index, first_chunk_size);
+    mergesort(items, b_index, second_chunk_size);
+
+    merge(items, start_index, first_chunk_size, b_index, second_chunk_size);
+}
+
+template <typename T>
+void mergesort(std::vector<T>& items_to_sort) {
+    mergesort(items_to_sort, 0, items_to_sort.size());
+}
+
+TEST(MergeSort, CheckEmpty) {
+    std::vector<int> items = { };
+    mergesort(items);
+    EXPECT_EQ(0, items.size());
+}
+
+TEST(MergeSort, SortEven) {
+    std::vector<int> items = { 6, 2, -100, 464 };
+    mergesort(items);
+    EXPECT_EQ(-100, items[0]);
+    EXPECT_EQ(2, items[1]);
+    EXPECT_EQ(6, items[2]);
+    EXPECT_EQ(464, items[3]);
+}
+
+TEST(MergeSort, SortOdd) {
+    std::vector<int> items = { 6, 2, -100, 464, 12 };
+    mergesort(items);
+    EXPECT_EQ(-100, items[0]);
+    EXPECT_EQ(2, items[1]);
+    EXPECT_EQ(6, items[2]);
+    EXPECT_EQ(12, items[3]);
+    EXPECT_EQ(464, items[4]);
+}
+
+TEST(MergeSort, AnotherTest) {
+    std::vector<int> items = { 6, 5, 4, 3, 2, 1 };
+    mergesort(items);
+    EXPECT_EQ(1, items[0]);
+    EXPECT_EQ(2, items[1]);
+    EXPECT_EQ(3, items[2]);
+    EXPECT_EQ(4, items[3]);
+    EXPECT_EQ(5, items[4]);
+    EXPECT_EQ(6, items[5]);
+}
