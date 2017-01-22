856e12ddce71d39b2e23f2282f40ce4fc8c9fa26
Daniel Phillips <dphildev@gmail.com>
2017-01-22 22:05:57 +0000

    Added BFS support for the two graph types and fixed the adjacency matrix code so it supports multiple edges between vertices.

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <queue>
 #include "gtest/gtest.h"
 
-class AdjacencyMatrixGraph
+class IGraph
+{
+public:
+    virtual ~IGraph() = default;
+    virtual void AddEdge(int from, int to, bool directed) = 0;
+    virtual void RemoveEdge(int from, int to, bool directed) = 0;
+    virtual std::vector<int> GetEdgesForVertex(int vertex) = 0;
+    virtual int GetVertexCount() const = 0;
+};
+
+class AdjacencyMatrixGraph : public IGraph
 {
 public:
     AdjacencyMatrixGraph(int number_of_vertices) {
@@ -13,24 +24,24 @@ public:
         }
     }
 
-    void AddEdge(int from, int to, bool directed) {
+    virtual void AddEdge(int from, int to, bool directed) override {
         m_matrix[from][to] += 1;
         if (!directed) {
             AddEdge(to, from, true);
         }
     }
 
-    void RemoveEdge(int from, int to, bool directed) {
+    virtual void RemoveEdge(int from, int to, bool directed) override {
         m_matrix[from][to] = std::max(m_matrix[from][to] - 1, 0);
         if (!directed) {
             m_matrix[to][from] = std::max(m_matrix[to][from] - 1, 0);
         }
     }
 
-    std::vector<int> GetEdgesForVertex(int vertex) {
+    virtual std::vector<int> GetEdgesForVertex(int vertex) override {
         std::vector<int> result;
         for (int i = 0; i < m_matrix.size(); i++) {
-            if (m_matrix[vertex][i] > 0) {
+            for (int j = 0; j < m_matrix[vertex][i]; j++) {
                 result.push_back(i);
             }
         }
@@ -38,12 +49,16 @@ public:
         return result;
     }
 
+    virtual int GetVertexCount() const override {
+        return m_matrix.size();
+    }
+
 private:
 
     std::vector<std::vector<int>> m_matrix;
 };
 
-class AdjacencyListGraph
+class AdjacencyListGraph : public IGraph
 {
 private:
     class Edge {
@@ -61,14 +76,14 @@ public:
         }
     }
 
-    void AddEdge(int from, int to, bool directed) {
+    virtual void AddEdge(int from, int to, bool directed) override {
         m_lists[from].push_back(Edge(to));
         if (!directed) {
             AddEdge(to, from, true);
         }
     }
 
-    void RemoveEdge(int from, int to, bool directed) {
+    virtual void RemoveEdge(int from, int to, bool directed) override {
         auto& list = m_lists[from];
         auto to_remove = std::find_if(list.begin(), list.end(), [to] (const Edge& e) { return e.m_to == to; } );
         if (to_remove != list.end()) {
@@ -80,7 +95,7 @@ public:
         }
     }
 
-    std::vector<int> GetEdgesForVertex(int vertex) {
+    virtual std::vector<int> GetEdgesForVertex(int vertex) override {
         std::vector<int> result;
         for (const auto& v : m_lists[vertex]) {
             result.push_back(v.m_to);
@@ -89,14 +104,60 @@ public:
         return result;
     }
 
+    virtual int GetVertexCount() const override {
+        return m_lists.size();
+    }
+
 private:
 
     std::vector<std::vector<Edge>> m_lists;
 };
 
+void bfs(
+    IGraph* graph,
+    const std::function<void (int, int)>& process_edge
+) {
+    enum class VertexState
+    {
+        Undiscovered,
+        Discovered,
+        Processed
+    };
+
+    // initialise the tables of processed and unprocessed vertices
+    const auto vertex_count = graph->GetVertexCount();
+    if (vertex_count <= 0) {
+        return;
+    }
+
+    std::vector<VertexState> state(vertex_count, VertexState::Undiscovered);
+    std::vector<int> parents(vertex_count, -1);
+
+    std::queue<int> to_process;
+    to_process.push(0);
+    state[0] = VertexState::Discovered;
+
+    while (!to_process.empty()) {
+        const auto vertex = to_process.front();
+        to_process.pop();
+        state[vertex] = VertexState::Processed;
+
+        const auto edges = graph->GetEdgesForVertex(vertex);
+        for (auto edge : edges) {
+            if (state[edge] != VertexState::Processed) {
+                process_edge(vertex, edge);
+            }
+            if (state[edge] == VertexState::Undiscovered) {
+                state[edge] = VertexState::Discovered;
+                to_process.push(edge);
+                parents[edge] = vertex;
+            }
+        }
+    }
+}
+
 template <typename T>
 class GraphTest : public ::testing::Test {
-
 };
 
 typedef ::testing::Types<AdjacencyListGraph, AdjacencyMatrixGraph> GraphTypes;
@@ -135,4 +196,19 @@ TYPED_TEST(GraphTest, TestRemoveEdge) {
 
     EXPECT_TRUE(edges3.empty());
     EXPECT_TRUE(edges4.empty());
+}
+
+TYPED_TEST(GraphTest, TestBFS) {
+    TypeParam graph(5);
+    graph.AddEdge(0, 4, false);
+    graph.AddEdge(4, 0, false);
+
+    std::vector<std::pair<int, int>> edges;
+    bfs(&graph, [&] (int from, int to) {
+        edges.push_back(std::make_pair(from, to));
+    });
+
+    EXPECT_EQ(2, edges.size());
+    EXPECT_EQ(std::make_pair(0, 4), edges[0]);
+    EXPECT_EQ(std::make_pair(0, 4), edges[0]);
 }
